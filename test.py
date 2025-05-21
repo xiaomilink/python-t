0716fcf44f678423ecd9ad9cc147fb72f8009646
xiaomilink <1565270738@qq.com>
2025-05-21 21:36:10 +0800

    test 2266

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 def safe_add(a: float, b: float) -> float:
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("参数必须是数字类型")
+        raise TypeError("参数必须是数字类型999")
     return float(a) + float(b)
